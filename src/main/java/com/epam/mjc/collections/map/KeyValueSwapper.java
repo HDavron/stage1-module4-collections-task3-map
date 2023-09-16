b1567b8d5af5b1f55137d5c0756b2f14f8aa513e
Davron <hdavron7@gmail.com>
2023-09-16 21:04:04 +0500

    task3

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+
+        sourceMap.forEach((key, value) -> {
+            map.merge(value, key, Integer::min);
+        });
+
+        return map;
     }
 }
